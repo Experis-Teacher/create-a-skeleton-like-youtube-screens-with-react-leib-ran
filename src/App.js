00d9c659e340ea1b5fcffa1d2e30a8710b45c65b
Ran Leibovitz <leibovitzran@gmail.com>
2021-08-31 11:55:11 +0300

    sekalton1

--- src/App.js
+++ src/App.js
@@ -1,58 +1,78 @@
-import './App.css';
+import "./App.css";
 import dummyData from "./data";
-import CardList from './Components/CardList';
-import { Component } from 'react';
-import SkeletonCard from './Components/SkeletonCard';
+import CardList from "./Components/CardList";
+import { Component } from "react";
+import SkeletonCard from "./Components/SkeletonCard";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 
 class App extends Component {
-  
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       videos: [],
       loading: false,
-      timer: null
-    }
+      timer: null,
+    };
   }
 
-  setVideosState(videos = []){
-    this.setState({videos})
+  setVideosState(videos = []) {
+    this.setState({ videos });
   }
 
-  setLoadingState(value = false){
-    this.setState({loading: value})
+  setLoadingState(value = false) {
+    this.setState({ loading: value });
   }
-  
 
-  componentDidMount(){
+  componentDidMount() {
     this.setLoadingState(true);
     const timer = setTimeout(() => {
       this.setVideosState(dummyData);
       this.setLoadingState(false);
     }, 5000);
-    this.setState({timer})
+    this.setState({ timer });
   }
 
-  componentWillUnmount(){
-    clearTimeout(this.state.timer)
+  componentWillUnmount() {
+    clearTimeout(this.state.timer);
   }
 
+  listgenrator(card) {
+    let list = [];
+    for (let index = 0; index < 3; index++) {
+      list.push(card);
+    }
+    console.log(card);
+    return list;
+  }
 
-
-  render(){
+  render() {
     return (
       <div className="App">
-         {/* {
-          this.state.videos.map((list, index) => {
-            return (
-              <section key={index}>
-                <h2 className="section-title">{list.section}</h2>
-                <CardList list={list} />
-                <hr />
-              </section>
-            );
-          })} */}
-          <SkeletonCard/>
+        {this.state.videos.map((list, index) => {
+          return (
+            <section key={index}>
+              <h2 className="section-title">{list.section}</h2>
+              <CardList list={list} />
+              <hr />
+            </section>
+          );
+        })}
+        <SkeletonTheme color="grey" highlightColor="#444">
+          <p>
+            <Skeleton height={32} width={200} count={1} />
+          </p>
+        </SkeletonTheme>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          {this.listgenrator(<SkeletonCard style={{ width: 360 }} />)}
+        </div>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          {this.listgenrator(<SkeletonCard style={{ width: 360 }} />)}
+        </div>
+        <SkeletonTheme color="grey" highlightColor="#444">
+          <p>
+            <Skeleton height={2} width={1000} count={1} />
+          </p>
+        </SkeletonTheme>
       </div>
     );
   }
--- src/Components/SkeletonCard.jsx
+++ src/Components/SkeletonCard.jsx
@@ -1,24 +1,49 @@
-import React from 'react'
+import React from "react";
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 
 const SkeletonCard = () => {
-    return (
-        <div>
-                        
-            <SkeletonTheme color="grey" highlightColor="#444">
-            <p>
-                <Skeleton height={250} width={300} count={1} />
-            </p>
+  return (
+    <div>
+      <SkeletonTheme color="grey" highlightColor="#444">
+        <p>
+          <Skeleton height={172} width={306} count={1} />
+        </p>
+      </SkeletonTheme>
 
-            </SkeletonTheme>
-            <SkeletonTheme color="#990" highlightColor="#550">
-            <p>
-                <Skeleton height={250} width={300} count={1} />
-            </p>
+      <SkeletonTheme color="grey" highlightColor="#444">
+        <p>
+          <Skeleton circle={true} height={40} width={40} count={1} />
+        </p>
+      </SkeletonTheme>
 
-            </SkeletonTheme>
-        </div>
-    )
-}
+      <SkeletonTheme color="gray" highlightColor="#444">
+        <p>
+          <Skeleton height={42} width={306} count={1} />
+        </p>
+      </SkeletonTheme>
+      <SkeletonTheme color="gray" highlightColor="#444">
+        <p>
+          <Skeleton height={19} width={90} count={1} />
+        </p>
+      </SkeletonTheme>
+      <SkeletonTheme color="gray" highlightColor="#444" style={{ margin: "2" }}>
+        <p>
+          <Skeleton height={21} width={306} count={1} />
+        </p>
+      </SkeletonTheme>
+      <SkeletonTheme color="white">
+        <p>
+          <Skeleton height={21} width={320} count={1} />
+        </p>
+      </SkeletonTheme>
 
-export default SkeletonCard
+      {/* <SkeletonTheme color="gray" highlightColor="#444">
+        <p>
+          <Skeleton height={2} width={960} count={1} />
+        </p>
+      </SkeletonTheme> */}
+    </div>
+  );
+};
+
+export default SkeletonCard;
